0c759ffee089fdaf55f6db3581b08e6e6c1d8c8c
Marián <Matiasus@users.noreply.github.com>
2019-11-08 08:37:37 +0100

    Update hd44780.c

--- lib/hd44780.c
+++ lib/hd44780.c
@@ -25,7 +25,7 @@
  * @return  void
  */
 void HD44780_Init(EMode mode)
-{ 
+{
   // set DB7-DB4 as output
   SETBIT(HD44780_DDR_DB, DB7);
   SETBIT(HD44780_DDR_DB, DB6);
@@ -38,12 +38,12 @@ void HD44780_Init(EMode mode)
     SETBIT(HD44780_DDR_DB, HD44780_DB1);
     SETBIT(HD44780_DDR_DB, HD44780_DB0);
   }
-  
-  
-  // Initializing LCD Driver HD44780 according to Figure 24 
+
+
+  // Initializing LCD Driver HD44780 according to Figure 24
   // in https://www.sparkfun.com/datasheets/LCD/HD44780.pdf
   //
-  // -----------------------------  
+  // -----------------------------
   // |         Power on          |
   // -----------------------------
   //              |
@@ -51,147 +51,96 @@ void HD44780_Init(EMode mode)
   // | Wait for more than 15 ms  |
   // | after VCC rises to 4.5 V  |
   // -----------------------------
+  _delay_ms(15);
   //              |
   // -----------------------------
   // |  RS R/W DB7 DB6 DB5 DB4   |
   // |   0   0   0   0   1   1   |
   // -----------------------------
+  HD44780_SendInstruction(HD44780_INIT_SEQ, mode);
   //              |
   // -----------------------------
   // | Wait for more than 4.1 ms |
   // -----------------------------
+  _delay_ms(4.1);
   //              |
   // -----------------------------
   // |  RS R/W DB7 DB6 DB5 DB4   |
   // |   0   0   0   0   1   1   |
   // -----------------------------
+  HD44780_SendInstruction(HD44780_INIT_SEQ, mode);
   //              |
   // -----------------------------
   // | Wait for more than 0.1 ms |
-  // -----------------------------  
+  // -----------------------------
+  _delay_us(100);
   //              |
   // -----------------------------
   // |  RS R/W DB7 DB6 DB5 DB4   |
   // |   0   0   0   0   1   1   |
   // -----------------------------
+  HD44780_SendInstruction(HD44780_INIT_SEQ, mode);
   //              |
-  // -----------------------------
-  // | Wait for more than 41 us  |
-  // -----------------------------
+  // -----------------------------   // 37 us writing DATA into DDRAM or CGRAM
+  // | Wait for more than 41 us  |   //  4 us tadd - time after busy flag disapeared
+  // -----------------------------   // 37 us + 4 us = 41 us
+  _delay_us(41);
   //              |
   // -----------------------------   // 4 bit operation
-  // |  RS R/W DB7 DB6 DB5 DB4   |
+  // |  RS R/W DB7 DB6 DB5 DB4   |   // 0x20
   // |   0   0   0   0   1   0   |
   // -----------------------------
+  HD44780_SendInstruction(HD44780_4BIT_MODE, mode);
   //              |
-  // -----------------------------
-  // | Wait for more than 41 us  |
-  // -----------------------------
+  // -----------------------------   // 37 us writing DATA into DDRAM or CGRAM
+  // | Wait for more than 41 us  |   //  4 us tadd - time after busy flag disapeared
+  // -----------------------------   // 37 us + 4 us = 41 us
+  _delay_us(41);
   //              |
   // -----------------------------   // Display off
-  // |  RS R/W DB7 DB6 DB5 DB4   |   // 
+  // |  RS R/W DB7 DB6 DB5 DB4   |   //
   // |   0   0   0   0   0   0   |   // D - display: 0 - no, 1 - yes
   // |   0   0   1   D   C   B   |   // C - cursor: 0 - no, 1 - yes
   // -----------------------------   // B - blink cursor: 0 - no, 1 - yes
+  //  Upper nibble 0000 -> 0x00
+  HD44780_SendInstruction(0x00, mode);
+  //  Lower nibble 1000 -> 0x80
+  HD44780_SendInstruction(0x80, mode);
   //              |
-  // -----------------------------
-  // | Wait for more than 41 us  |
-  // -----------------------------
+  // -----------------------------   // 37 us writing DATA into DDRAM or CGRAM
+  // | Wait for more than 41 us  |   //  4 us tadd - time after busy flag disapeared
+  // -----------------------------   // 37 us + 4 us = 41 us
+  _delay_us(41);
   //              |
   // -----------------------------   // Display clear
   // |  RS R/W DB7 DB6 DB5 DB4   |   // 0x01
   // |   0   0   0   0   0   0   |
   // |   0   0   0   0   0   1   |
   // -----------------------------
+  //  Upper nibble 0000 -> 0x00
+  HD44780_SendInstruction(0x00, mode);
+  //  Lower nibble 0001 -> 0x10
+  HD44780_SendInstruction(0x10, mode);
   //              |
   // -----------------------------
   // | Wait for more than 41 us  |
-  // -----------------------------  
+  // -----------------------------
+  _delay_us(41);
   //              |
   // -----------------------------   // Entry mode set
-  // |  RS R/W DB7 DB6 DB5 DB4   |   // 
+  // |  RS R/W DB7 DB6 DB5 DB4   |   //
   // |   0   0   0   0   0   0   |   // I/D - shift cursor  (1 - left, 0 - right)
-  // |   0   0   0   1 I/D   S   |   // S - shift text: 1 - no, 0 - yes  
+  // |   0   0   0   1 I/D   S   |   // S - shift text: 1 - no, 0 - yes
   // -----------------------------
+  //  Upper nibble 0000 -> 0x00
+  HD44780_SendInstruction(0x00, mode);
+  //  Lower nibble 0110 -> 0x60
+  HD44780_SendInstruction(0x60, mode);
   //              |
   // -----------------------------
   // | Wait for more than 41 us  |
-  // -----------------------------   
-  
-  // Wait for more than 15 ms after VCC rises to 4.5 V
-  _delay_ms(15);
-  // Init sequence
-  HD44780_SendInstruction(HD44780_INIT_SEQ, mode);
-  // Wait for more than 4.1 ms
-  _delay_ms(4.1);
-  // Init sequence
-  HD44780_SendInstruction(0x30, mode);
-  // Wait for more than 100 us
-  _delay_us(100);
-  // Init sequence
-  HD44780_SendInstruction(0x30, mode);
-  // When BF is not checked, the waiting time between instructions
-  // is longer than the execution instuction time.
-  //
-  // 37 us writing DATA into DDRAM or CGRAM
-  //  4 us tadd - time after busz flag disapeared
-  // --------------------------------------------
-  // t > 37 us + 4 us
+  // -----------------------------
   _delay_us(41);
-  
-  // Sequence for 4bit operation
-  //  RS R/W DB7 DB6 DB5 DB4
-  //   0   0   0   0   1   0
-  HD44780_SendInstruction(0x20, mode);
-  // Wait for more than 37 us + 4 us
-  _delay_us(41);
-
-  // Display off
-  // ---------------------------------------
-  // DB7 DB6 DB5 DB4 DB3 DB2 DB1 DB0
-  // 0   0   0   0   1   D   C   B
-  //
-  // D - display: 0 - no, 1 - yes
-  // C - cursor: 0 - no, 1 - yes
-  // B - blink cursor: 0 - no, 1 - yes
-  // ---------------------------------------
-  //  RS R/W DB7 DB6 DB5 DB4
-  //   0   0   0   0   0   0 - upper nibble
-  HD44780_SendInstruction(0x00, mode);
-  //   0   0   1   0   0   0 - lower nibble
-  HD44780_SendInstruction(0x80, mode);
-  // Wait for more than 37 us + 4 us
-  _delay_us(41);
-
-  // Display clear
-  // ---------------------------------------
-  // DB7 DB6 DB5 DB4 DB3 DB2 DB1 DB0
-  // 0   0   0   0   0   0   0   1
-  //
-  // ---------------------------------------
-  //  RS R/W DB7 DB6 DB5 DB4
-  //   0   0   0   0   0   0 - upper nibble
-  HD44780_SendInstruction(0x00, mode);
-  //   0   0   0   0   0   1 - lower nibble
-  HD44780_SendInstruction(0x10, mode);
-  // Wait for more than 37 us + 4 us
-  _delay_us(41);
-  
-  // Entry mode set
-  // ---------------------------------------
-  // DB7 DB6 DB5 DB4 DB3 DB2 DB1 DB0
-  // 0   0   0   0   0   1   I/D   S
-  //
-  // I/D - shift cursor: 1 - left, 0 - right
-  //   S - shift text: 1 - no, 0 - yes
-  // ---------------------------------------
-  //  RS R/W DB7 DB6 DB5 DB4
-  //   0   0   0   0   0   0 - upper nibble
-  HD44780_SendInstruction(0x00, mode);
-  //   0   0   0   1   1   0 - lower nibble
-  HD44780_SendInstruction(0x60, mode);  
-  // Wait for more than 37 us + 4 us
-  _delay_us(41);  
 }
 
 /**
@@ -211,7 +160,7 @@ void HD44780_OutUppNibble(unsigned short int data)
   SET_IF_BIT_IN_DATA_SET(HD44780_PORT_DB, HD44780_DB7, data, BIT7);
   SET_IF_BIT_IN_DATA_SET(HD44780_PORT_DB, HD44780_DB6, data, BIT6);
   SET_IF_BIT_IN_DATA_SET(HD44780_PORT_DB, HD44780_DB5, data, BIT5);
-  SET_IF_BIT_IN_DATA_SET(HD44780_PORT_DB, HD44780_DB4, data, BIT4); 
+  SET_IF_BIT_IN_DATA_SET(HD44780_PORT_DB, HD44780_DB4, data, BIT4);
 }
 
   /**
@@ -222,7 +171,7 @@ void HD44780_OutUppNibble(unsigned short int data)
  */
 void HD44780_OutLowNibble(unsigned short int data)
 {
-  // clear bits DB3-DB0  
+  // clear bits DB3-DB0
   CLRBIT(HD44780_PORT_DB, HD44780_DB3);
   CLRBIT(HD44780_PORT_DB, HD44780_DB2);
   CLRBIT(HD44780_PORT_DB, HD44780_DB1);
@@ -231,7 +180,7 @@ void HD44780_OutLowNibble(unsigned short int data)
   SET_IF_BIT_IN_DATA_SET(HD44780_PORT_DB, HD44780_DB3, data, BIT3);
   SET_IF_BIT_IN_DATA_SET(HD44780_PORT_DB, HD44780_DB2, data, BIT2);
   SET_IF_BIT_IN_DATA_SET(HD44780_PORT_DB, HD44780_DB1, data, BIT1);
-  SET_IF_BIT_IN_DATA_SET(HD44780_PORT_DB, HD44780_DB0, data, BIT0);  
+  SET_IF_BIT_IN_DATA_SET(HD44780_PORT_DB, HD44780_DB0, data, BIT0);
 }
 
 /**
@@ -249,7 +198,7 @@ void HD44780_OutData(unsigned short int data, EMode mode)
     // Send out lower nibble
     HD44780_OutLowNibble(data);
   }
-  // pulse E 
+  // pulse E
   HD44780_PulseE();
 }
 
