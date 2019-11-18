48a3764310c87aea7eedb623d922b2bb2ba512f1
Marián <Matiasus@users.noreply.github.com>
2019-11-18 20:21:26 +0100

    Add files via upload

--- lib/hd44780.c
+++ lib/hd44780.c
@@ -14,9 +14,92 @@
 
 // include libraries
 #include <stdio.h>
-#include <dalay.h>
+#include <util/delay.h>
 #include <avr/io.h>
 #include "hd44780.h"
+
+// Initializing LCD Driver HD44780 according to Figure 24
+// in https://www.sparkfun.com/datasheets/LCD/HD44780.pdf
+//
+// -----------------------------
+// |         Power on          |
+// -----------------------------
+//              |
+// -----------------------------
+// | Wait for more than 15 ms  |
+// | after VCC rises to 4.5 V  |
+// -----------------------------
+//              |
+// -----------------------------
+// |  RS R/W DB7 DB6 DB5 DB4   |
+// |   0   0   0   0   1   1   |
+// -----------------------------
+//              |
+// -----------------------------
+// | Wait for more than 4.1 ms |
+// -----------------------------
+//              |
+// -----------------------------
+// |  RS R/W DB7 DB6 DB5 DB4   |
+// |   0   0   0   0   1   1   |
+// -----------------------------
+//              |
+// -----------------------------
+// | Wait for more than 0.1 ms |
+// -----------------------------
+//              |
+// -----------------------------
+// |  RS R/W DB7 DB6 DB5 DB4   |
+// |   0   0   0   0   1   1   |
+// -----------------------------
+//              |
+// -----------------------------   // 37 us writing DATA into DDRAM or CGRAM
+// | Wait for more than 41 us  |   //  4 us tadd - time after busy flag disapeared
+// -----------------------------   // 37 us + 4 us = 41 us
+//              |
+// -----------------------------   // 4 bit operation
+// |  RS R/W DB7 DB6 DB5 DB4   |   // 0x20
+// |   0   0   0   0   1   0   |
+// -----------------------------
+//              |
+// -----------------------------   // 37 us writing DATA into DDRAM or CGRAM
+// | Wait for more than 41 us  |   //  4 us tadd - time after busy flag disapeared
+// -----------------------------   // 37 us + 4 us = 41 us
+//              |
+// -----------------------------   // Display off
+// |  RS R/W DB7 DB6 DB5 DB4   |   //
+// |   0   0   0   0   0   0   |   // D - display: 0 - no, 1 - yes
+// |   0   0   1   D   C   B   |   // C - cursor: 0 - no, 1 - yes
+// -----------------------------   // B - blink cursor: 0 - no, 1 - yes
+//              |
+// -----------------------------   // 37 us writing DATA into DDRAM or CGRAM
+// | Wait for more than 41 us  |   //  4 us tadd - time after busy flag disapeared
+// -----------------------------   // 37 us + 4 us = 41 us
+//              |
+// -----------------------------   // Display clear
+// |  RS R/W DB7 DB6 DB5 DB4   |   // 0x01
+// |   0   0   0   0   0   0   |
+// |   0   0   0   0   0   1   |
+// -----------------------------
+//              |
+// -----------------------------
+// | Wait for more than 41 us  |
+// -----------------------------
+//              |
+// -----------------------------   // Entry mode set
+// |  RS R/W DB7 DB6 DB5 DB4   |   //
+// |   0   0   0   0   0   0   |   // I/D - shift cursor  (1 - left, 0 - right)
+// |   0   0   0   1 I/D   S   |   // S - shift text: 1 - no, 0 - yes
+// -----------------------------
+//              |
+// -----------------------------
+// | Wait for more than 41 us  |
+// -----------------------------
+
+unsigned short int HD44780_mode = e4BIT;
+
+
+
 
 /**
  * @desc    LCD init - initialisation routine
@@ -24,172 +107,227 @@
  * @param   EMode
  * @return  void
  */
-void HD44780_Init(EMode mode)
+void HD44780_Init (EMode mode)
 {
+  // set E as output
+  SETBIT(HD44780_DDR_E, HD44780_E);
+  // set RS as output
+  SETBIT(HD44780_DDR_RS, HD44780_RS);
+
   // set DB7-DB4 as output
-  SETBIT(HD44780_DDR_DB, DB7);
-  SETBIT(HD44780_DDR_DB, DB6);
-  SETBIT(HD44780_DDR_DB, DB5);
-  SETBIT(HD44780_DDR_DB, DB4);
-  // if 8 bit operation set DB3-DB0 as output
-  if (mode == e8BIT) {
-    SETBIT(HD44780_DDR_DB, HD44780_DB3);
-    SETBIT(HD44780_DDR_DB, HD44780_DB2);
-    SETBIT(HD44780_DDR_DB, HD44780_DB1);
-    SETBIT(HD44780_DDR_DB, HD44780_DB0);
-  }
-  // Initializing LCD Driver HD44780 according to Figure 24
-  // in https://www.sparkfun.com/datasheets/LCD/HD44780.pdf
-  //
-  // -----------------------------
-  // |         Power on          |
-  // -----------------------------
-  //              |
-  // -----------------------------
-  // | Wait for more than 15 ms  |
-  // | after VCC rises to 4.5 V  |
-  // -----------------------------
+  SETBIT(HD44780_DDR_DB, HD44780_DB7);
+  SETBIT(HD44780_DDR_DB, HD44780_DB6);
+  SETBIT(HD44780_DDR_DB, HD44780_DB5);
+  SETBIT(HD44780_DDR_DB, HD44780_DB4);
+  
+  // clear RS
+  CLRBIT(HD44780_PORT_RS, HD44780_RS);
+  // clear RS
+  CLRBIT(HD44780_PORT_E, HD44780_E);
+
+  // set RS
+  SETBIT(HD44780_PORT_RS, HD44780_RS);
+  // set E
+  SETBIT(HD44780_PORT_E, HD44780_E);
+  // delay
   _delay_ms(15);
-  //              |
-  // -----------------------------
-  // |  RS R/W DB7 DB6 DB5 DB4   |
-  // |   0   0   0   0   1   1   |
-  // -----------------------------
-  HD44780_SendInstruction(HD44780_INIT_SEQ, mode);
-  //              |
-  // -----------------------------
-  // | Wait for more than 4.1 ms |
-  // -----------------------------
-  _delay_ms(4.1);
-  //              |
-  // -----------------------------
-  // |  RS R/W DB7 DB6 DB5 DB4   |
-  // |   0   0   0   0   1   1   |
-  // -----------------------------
-  HD44780_SendInstruction(HD44780_INIT_SEQ, mode);
-  //              |
-  // -----------------------------
-  // | Wait for more than 0.1 ms |
-  // -----------------------------
+  // clear RS
+  CLRBIT(HD44780_PORT_RS, HD44780_RS);
+  // clear E
+  CLRBIT(HD44780_PORT_E, HD44780_E);
+
+  // Initial sequence - send 4 bits in 4 bit mode
+  HD44780_SendInstruction(HD44780_Send4bitsIn4bitMode, HD44780_INIT_SEQ);
+  // delay
+  _delay_ms(5);
+
+  // pulse E
+  HD44780_PulseE();
+  // delay
+  _delay_ms(5);
+
+  // pulse E
+  HD44780_PulseE();
+  // delay
   _delay_us(100);
-  //              |
-  // -----------------------------
-  // |  RS R/W DB7 DB6 DB5 DB4   |
-  // |   0   0   0   0   1   1   |
-  // -----------------------------
-  HD44780_SendInstruction(HD44780_INIT_SEQ, mode);
-  //              |
-  // -----------------------------   // 37 us writing DATA into DDRAM or CGRAM
-  // | Wait for more than 41 us  |   //  4 us tadd - time after busy flag disapeared
-  // -----------------------------   // 37 us + 4 us = 41 us
-  _delay_us(41);
-  //              |
-  // -----------------------------   // 4 bit operation
-  // |  RS R/W DB7 DB6 DB5 DB4   |   // 0x20
-  // |   0   0   0   0   1   0   |
-  // -----------------------------
-  HD44780_SendInstruction(HD44780_4BIT_MODE, mode);
-  //              |
-  // -----------------------------   // 37 us writing DATA into DDRAM or CGRAM
-  // | Wait for more than 41 us  |   //  4 us tadd - time after busy flag disapeared
-  // -----------------------------   // 37 us + 4 us = 41 us
-  _delay_us(41);
-  //              |
-  // -----------------------------   // Display off
-  // |  RS R/W DB7 DB6 DB5 DB4   |   //
-  // |   0   0   0   0   0   0   |   // D - display: 0 - no, 1 - yes
-  // |   0   0   1   D   C   B   |   // C - cursor: 0 - no, 1 - yes
-  // -----------------------------   // B - blink cursor: 0 - no, 1 - yes
-  //  Upper nibble 0000 -> 0x00
-  HD44780_SendInstruction(0x00, mode);
-  //  Lower nibble 1000 -> 0x08
-  HD44780_SendInstruction(0x08, mode);
-  //              |
-  // -----------------------------   // 37 us writing DATA into DDRAM or CGRAM
-  // | Wait for more than 41 us  |   //  4 us tadd - time after busy flag disapeared
-  // -----------------------------   // 37 us + 4 us = 41 us
-  _delay_us(41);
-  //              |
-  // -----------------------------   // Display clear
-  // |  RS R/W DB7 DB6 DB5 DB4   |   // 0x01
-  // |   0   0   0   0   0   0   |
-  // |   0   0   0   0   0   1   |
-  // -----------------------------
-  //  Upper nibble 0000 -> 0x00
-  HD44780_SendInstruction(0x00, mode);
-  //  Lower nibble 0001 -> 0x10
-  HD44780_SendInstruction(0x01, mode);
-  //              |
-  // -----------------------------
-  // | Wait for more than 41 us  |
-  // -----------------------------
-  _delay_us(41);
-  //              |
-  // -----------------------------   // Entry mode set
-  // |  RS R/W DB7 DB6 DB5 DB4   |   //
-  // |   0   0   0   0   0   0   |   // I/D - shift cursor  (1 - left, 0 - right)
-  // |   0   0   0   1 I/D   S   |   // S - shift text: 1 - no, 0 - yes
-  // -----------------------------
-  //  Upper nibble 0000 -> 0x00
-  HD44780_SendInstruction(0x00, mode);
-  //  Lower nibble 0110 -> 0x60
-  HD44780_SendInstruction(0x06, mode);
-  //              |
-  // -----------------------------
-  // | Wait for more than 41 us  |
-  // -----------------------------
-  _delay_us(41);
+
+  // 4bit interfacing - send 4 bits in 4 bit mode
+  HD44780_SendInstruction(HD44780_Send4bitsIn4bitMode, HD44780_4BIT_MODE);
+  // delay
+  _delay_us(100);
+
+  // 4bit interfacing - send 8 bits in 4 bit mode
+  HD44780_SendInstruction(HD44780_Send8bitsIn4bitMode, HD44780_4BIT_MODE | HD44780_2_ROWS);
+  // delay
+  _delay_us(100);
+
+  // 4bit interfacing - send 8 bits in 4 bit mode
+  HD44780_SendInstruction(HD44780_Send8bitsIn4bitMode, 0x08);
+  // delay
+  _delay_us(100);
+
+  // 4bit interfacing - send 8 bits in 4 bit mode
+  HD44780_SendInstruction(HD44780_Send8bitsIn4bitMode, 0x01);
+  // delay
+  _delay_us(100);
+
+  // 4bit interfacing - send 8 bits in 4 bit mode
+  HD44780_SendInstruction(HD44780_Send8bitsIn4bitMode, 0x06);
+  // delay
+  _delay_us(100);
+
+}
+
+/**
+ * @desc    LCD display clear
+ *
+ * @param   void
+ * @return  void
+ */
+void HD44780_DisplayClear (void)
+{
+  // 4 bit mode
+  if (HD44780_mode == e4BIT) {
+    // Diplay clear - send 8 bits in 4 bit mode
+    HD44780_SendInstruction(HD44780_Send8bitsIn4bitMode, HD44780_DISP_CLEAR);
+  // 8 bit mode
+  } else {
+    // Diplay clear - send 8 bits in 8 bit mode
+    HD44780_SendInstruction(HD44780_Send8bitsIn8bitMode, HD44780_DISP_CLEAR);
+  }
+}
+
+/**
+ * @desc    LCD display on
+ *
+ * @param   void
+ * @return  void
+ */
+void HD44780_DisplayOn (void)
+{
+  // 4 bit mode
+  if (HD44780_mode == e4BIT) {
+    // Diplay on - send 8 bits in 4 bit mode
+    HD44780_SendInstruction(HD44780_Send8bitsIn4bitMode, HD44780_DISP_ON);
+  // 8 bit mode
+  } else {
+    // Diplay on - send 8 bits in 8 bit mode
+    HD44780_SendInstruction(HD44780_Send8bitsIn8bitMode, HD44780_DISP_ON);
+  }
+}
+
+/**
+ * @desc    LCD cursor on, display on
+ *
+ * @param   void
+ * @return  void
+ */
+void HD44780_CursorOn (void)
+{
+  // 4 bit mode
+  if (HD44780_mode == e4BIT) {
+    // Cursor on - send 8 bits in 4 bit mode
+    HD44780_SendInstruction(HD44780_Send8bitsIn4bitMode, HD44780_CURSOR_ON);
+  // 8 bit mode
+  } else {
+    // Cursor on - send 8 bits in 8 bit mode
+    HD44780_SendInstruction(HD44780_Send8bitsIn8bitMode, HD44780_CURSOR_ON);
+  }
+}
+
+/**
+ * @desc    LCD cursor blink, cursor on, display on
+ *
+ * @param   void
+ * @return  void
+ */
+void HD44780_CursorBlink (void)
+{
+  // 4 bit mode
+  if (HD44780_mode == e4BIT) {
+    // Cursor blink - send 8 bits in 4 bit mode
+    HD44780_SendInstruction(HD44780_Send8bitsIn4bitMode, HD44780_CURSOR_BLINK);
+  // 8 bit mode
+  } else {
+    // Cursor blink - send 8 bits in 8 bit mode
+    HD44780_SendInstruction(HD44780_Send8bitsIn8bitMode, HD44780_CURSOR_BLINK);
+  }
 }
 
 /**
  * @desc    LCD send instruction
  *
- * @param   unsigned short int
- * @param   EMode
+ * @param   void (*function) (unsigned short int)
  * @return  void
  */
-void HD44780_SendInstruction(unsigned short int data, EMode mode)
+void HD44780_SendInstruction (void (*SendBitMode) (unsigned short int data), unsigned short int data)
 {
   // Clear RS
-  CLRBIT(HD44780_PORT_RS, HD44780_RS);
-  // send data to LCD
-  HD44780_SendNibbles(data, mode);
+  HD44780_PORT_RS &= ~(1 << HD44780_RS);
+  // send required data in required mode
+  SendBitMode(data);
 }
 
 /**
- * @desc    LCD out data
+ * @desc    LCD send data
+ *
+ * @param   void (*function) (unsigned short int)
+ * @return  void
+ */
+void HD44780_SendData (void (*SendBitMode) (unsigned short int data), unsigned short int data)
+{
+  // Clear RS
+  HD44780_PORT_RS |= (1 << HD44780_RS);
+  // send required data in required mode
+  SendBitMode(data);
+  // Clear RS
+  HD44780_PORT_RS &= ~(1 << HD44780_RS);
+}
+
+/**
+ * @desc    LCD send 4bits instruction in 4 bit mode
  *
  * @param   unsigned short int
  * @return  void
  */
-void HD44780_SendData(unsigned short int data)
+void HD44780_Send4bitsIn4bitMode (unsigned short int data)
 {
-  // Set RS
-  SETBIT(HD44780_PORT_RS, HD44780_RS);
   // send data to LCD
-  HD44780_SendNibbles(data, mode);
-  // Clear RS
-  CLRBIT(HD44780_PORT_RS, HD44780_RS);
+  HD44780_SetUppNibble(data);
+  // pulse E
+  HD44780_PulseE();
 }
 
 /**
- * @desc    LCD out upper and lower nibbles
+ * @desc    LCD send 8bits instruction in 4 bit mode
  *
  * @param   unsigned short int
- * @param   EMode {e4BIT; e8BIT}
  * @return  void
  */
-void HD44780_SendNibbles(unsigned short int data, EMode mode)
+void HD44780_Send8bitsIn4bitMode (unsigned short int data)
 {
-  // Send out upper nibble
-  HD44780_SendUppNibble(data << mode);
-  // if 8bit mode
-  if (mode == e8BIT) {
-    // Send out upper nibble
-    HD44780_SendUppNibble(data & 0xf0);    
-    // Send out lower nibble
-    HD44780_SendLowNibble(data & 0x0f);
-  }
+  // send data to LCD
+  HD44780_SetUppNibble(data);
+  // pulse E
+  HD44780_PulseE();
+  // send data to LCD
+  HD44780_SetUppNibble(data << 4);
+  // pulse E
+  HD44780_PulseE();
+}
+
+/**
+ * @desc    LCD send 8bits instruction in 8 bit mode
+ *
+ * @param   unsigned short int
+ * @return  void
+ */
+void HD44780_Send8bitsIn8bitMode (unsigned short int data)
+{
+  // send data to LCD
+  HD44780_SetUppNibble(data);
+  // send data to LCD
+  HD44780_SetLowNibble(data);
   // pulse E
   HD44780_PulseE();
 }
@@ -200,7 +338,7 @@ void HD44780_SendNibbles(unsigned short int data, EMode mode)
  * @param   unsigned short int
  * @return  void
  */
-void HD44780_SendUppNibble(unsigned short int data)
+void HD44780_SetUppNibble (unsigned short int data)
 {
   // clear bits DB7-DB4
   CLRBIT(HD44780_PORT_DB, HD44780_DB7);
@@ -208,30 +346,32 @@ void HD44780_SendUppNibble(unsigned short int data)
   CLRBIT(HD44780_PORT_DB, HD44780_DB5);
   CLRBIT(HD44780_PORT_DB, HD44780_DB4);
   // set DB7-DB4 if corresponding bit is set
-  SET_IF_BIT_IN_DATA_SET(HD44780_PORT_DB, HD44780_DB7, data, BIT7);
-  SET_IF_BIT_IN_DATA_SET(HD44780_PORT_DB, HD44780_DB6, data, BIT6);
-  SET_IF_BIT_IN_DATA_SET(HD44780_PORT_DB, HD44780_DB5, data, BIT5);
-  SET_IF_BIT_IN_DATA_SET(HD44780_PORT_DB, HD44780_DB4, data, BIT4);
+  if (data & 0x80) { SETBIT(HD44780_PORT_DB, HD44780_DB7); }
+  if (data & 0x40) { SETBIT(HD44780_PORT_DB, HD44780_DB6); }
+  if (data & 0x20) { SETBIT(HD44780_PORT_DB, HD44780_DB5); }
+  if (data & 0x10) { SETBIT(HD44780_PORT_DB, HD44780_DB4); }
 }
 
 /**
- * @desc    LCD send
+ * @desc    LCD send lower nibble
  *
  * @param   unsigned short int
  * @return  void
  */
-void HD44780_SendLowNibble(unsigned short int data)
+void HD44780_SetLowNibble (unsigned short int data)
 {
-  // clear bits DB3-DB0
-  CLRBIT(HD44780_PORT_DB, HD44780_DB3);
-  CLRBIT(HD44780_PORT_DB, HD44780_DB2);
-  CLRBIT(HD44780_PORT_DB, HD44780_DB1);
-  CLRBIT(HD44780_PORT_DB, HD44780_DB0);
-  // set DB3-DB0 if corresponding bit is set
-  SET_IF_BIT_IN_DATA_SET(HD44780_PORT_DB, HD44780_DB3, data, BIT3);
-  SET_IF_BIT_IN_DATA_SET(HD44780_PORT_DB, HD44780_DB2, data, BIT2);
-  SET_IF_BIT_IN_DATA_SET(HD44780_PORT_DB, HD44780_DB1, data, BIT1);
-  SET_IF_BIT_IN_DATA_SET(HD44780_PORT_DB, HD44780_DB0, data, BIT0);
+/*
+  // clear bits DB7-DB4
+  HD44780_PORT_DB &= ~(1 << HD44780_DB3);
+  HD44780_PORT_DB &= ~(1 << HD44780_DB2);
+  HD44780_PORT_DB &= ~(1 << HD44780_DB1);
+  HD44780_PORT_DB &= ~(1 << HD44780_DB0);
+  // set DB7-DB4 if corresponding bit is set
+  if (data & BIT3) { HD44780_PORT_DB |= (1 << HD44780_DB3); }
+  if (data & BIT2) { HD44780_PORT_DB |= (1 << HD44780_DB2); }
+  if (data & BIT1) { HD44780_PORT_DB |= (1 << HD44780_DB1); }
+  if (data & BIT0) { HD44780_PORT_DB |= (1 << HD44780_DB0); }
+*/
 }
 
 /**
@@ -240,12 +380,12 @@ void HD44780_SendLowNibble(unsigned short int data)
  * @param   void
  * @return  void
  */
-void HD44780_PulseE(void)
+void HD44780_PulseE (void)
 {
   // Set E
   SETBIT(HD44780_PORT_E, HD44780_E);
   // clock impuls > 0.5us
-  _delay_us(0.5);
+  _delay_us(0.6);
   // Clear E
   CLRBIT(HD44780_PORT_E, HD44780_E);
 }
--- lib/hd44780.h
+++ lib/hd44780.h
@@ -14,36 +14,37 @@
 #ifndef __HD44780_H__
 #define __HD44780_H__
 
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+
   // define clock
-  #if defined(__AVR_ATMEGA8__)
+  #if defined(__AVR_ATmega8__)
     #define _FCPU 8000000
-  #elif defined(__AVR_ATMEGA16__)
+  #elif defined(__AVR_ATmega16__)
     #define _FCPU 16000000
   #endif
 
-  // define register for HD44780 communication
-  #if defined(__AVR_ATMEGA8__) || defined(__AVR_ATMEGA16__)
-
+  #if defined(__AVR_ATmega16__)
     // E port
     #ifndef HD44780_DDR_E
-      #define HD44780_DDR_E   DDRC
+      #define HD44780_DDR_E   DDRB
     #endif  
     #ifndef HD44780_PORT_E
-      #define HD44780_PORT_E  PORTC
+      #define HD44780_PORT_E  PORTB
     #endif
     #ifndef HD44780_E
-      #define HD44780_E       PORT0
+      #define HD44780_E       3
     #endif      
     
     // RS port
     #ifndef HD44780_DDR_RS
-      #define HD44780_DDR_RS  DDRC
+      #define HD44780_DDR_RS  DDRB
     #endif
     #ifndef HD44780_PORT_RS
-      #define HD44780_PORT_RS PORTC
+      #define HD44780_PORT_RS PORTB
     #endif    
     #ifndef HD44780_RS
-      #define HD44780_RS      PORT1
+      #define HD44780_RS      2
     #endif
     
     // DATA port 
@@ -54,30 +55,32 @@
       #define HD44780_PORT_DB  PORTB
     #endif    
     #ifndef HD44780_DB7    
-      #define HD44780_DB7 PORT7 // LCD PORT DB7
+      #define HD44780_DB7 7 // LCD PORT DB7
     #endif
     #ifndef HD44780_DB6
-      #define HD44780_DB6 PORT6 // LCD PORT DB6
+      #define HD44780_DB6 6 // LCD PORT DB6
     #endif
     #ifndef HD44780_DB5    
-      #define HD44780_DB5 PORT5 // LCD PORT DB5
+      #define HD44780_DB5 5 // LCD PORT DB5
     #endif
     #ifndef HD44780_DB4    
-      #define HD44780_DB4 PORT4 // LCD PORT DB4
+      #define HD44780_DB4 4 // LCD PORT DB4
     #endif
+/*
     #ifndef HD44780_DB3    
-      #define HD44780_DB3 PORT3 // LCD PORT DB3
+      #define HD44780_DB3 3 // LCD PORT DB3
     #endif
     #ifndef HD44780_DB2    
-      #define HD44780_DB2 PORT2 // LCD PORT DB2
+      #define HD44780_DB2 2 // LCD PORT DB2
     #endif
     #ifndef HD44780_DB1    
-      #define HD44780_DB1 PORT1 // LCD PORT DB1
+      #define HD44780_DB1 1 // LCD PORT DB1
     #endif
     #ifndef HD44780_DB0    
-      #define HD44780_DB0 PORT0 // LCD PORT DB0
-    #endif      
-  #endif
+      #define HD44780_DB0 0 // LCD PORT DB0
+    #endif   
+*/  
+  #endif  
   
   #define BIT7 0x80
   #define BIT6 0x40
@@ -87,28 +90,32 @@
   #define BIT2 0x04
   #define BIT1 0x02
   #define BIT0 0x01
-
-  #define HD44780_4BIT_INIT_SEQ     0x03
-  #define HD44780_8BIT_INIT_SEQ     0x30
   
+  #define HD44780_INIT_SEQ     0x30
   #define HD44780_DISP_CLEAR   0x01
   #define HD44780_DISP_OFF     0x08
   #define HD44780_DISP_ON      0x0C
   #define HD44780_CURSOR_ON    0x0E
   #define HD44780_CURSOR_BLINK 0x0F  
   #define HD44780_ENTRY_MODE   0x06
-  #define HD44780_4BIT_MODE    0x02
+  #define HD44780_4BIT_MODE    0x20
+  #define HD44780_8BIT_MODE    0x30
+  #define HD44780_2_ROWS       0x08
+  #define HD44780_FONT_5x10    0x04
+
+  #define HD44780_ROW1_START   0x00
+  #define HD44780_ROW2_START   0x40
   
   // set bit
   #define SETBIT(REG, BIT) { REG |= (1 << BIT); }
   // clear bit
-  #define CLRBIT(REG, BIT) { REG &= (1 << BIT); }
+  #define CLRBIT(REG, BIT) { REG &= ~(1 << BIT); }
   // set port / pin if bit is set
-  #define SET_IF_BIT_IN_DATA_SET(PORT, PIN, BYTE, BIT) { if(BYTE & BIT) {SETBIT(PORT, PIN)} };
+  #define SET_IF_BIT_IS_SET(REG, PORT, DATA, BIT) { if((DATA & BIT) > 0) { SETBIT(REG, PORT); } }
   
   /** @enum Type of operation read / write */
   typedef enum {
-    e4BIT = 4;
+    e4BIT = 4,
     e8BIT = 0
   } EMode;
   
@@ -121,30 +128,76 @@
   void HD44780_Init(EMode);
 
   /**
+   * @desc    LCD display clear
+   *
+   * @param   void
+   * @return  void
+   */
+  void HD44780_DisplayClear (void);
+
+  /**
+   * @desc    LCD display on
+   *
+   * @param   void
+   * @return  void
+   */
+  void HD44780_DisplayOn (void);
+
+  /**
+   * @desc    LCD cursor on, display on
+   *
+   * @param   void
+   * @return  void
+   */
+  void HD44780_CursorOn (void);
+
+  /**
+   * @desc    LCD cursor blink, cursor on, display on
+   *
+   * @param   void
+   * @return  void
+   */
+  void HD44780_CursorBlink (void);
+
+  /**
    * @desc    LCD send instruction
    *
-   * @param   unsigned short int
-   * @param   EMode
+   * @param   void (*function) (unsigned short int)
    * @return  void
    */
-  void HD44780_SendInstruction(unsigned short int, EMode);
+  void HD44780_SendInstruction (void (*SendBitMode) (unsigned short int), unsigned short int);
 
   /**
-   * @desc    LCD out data
+   * @desc    LCD send data
+   *
+   * @param   void (*function) (unsigned short int)
+   * @return  void
+   */
+  void HD44780_SendData (void (*SendBitMode) (unsigned short int), unsigned short int);
+
+  /**
+   * @desc    LCD send 4bits instruction in 4 bit mode
    *
    * @param   unsigned short int
    * @return  void
    */
-  void HD44780_SendData(unsigned short int);
+  void HD44780_Send4bitsIn4bitMode (unsigned short int);
 
   /**
-   * @desc    LCD out upper and lower nibbles
+   * @desc    LCD send 8bits instruction in 4 bit mode
    *
    * @param   unsigned short int
-   * @param   EMode {e4BIT; e8BIT}
    * @return  void
    */
-  void HD44780_SendNibbles(unsigned short int, EMode);
+  void HD44780_Send8bitsIn4bitMode (unsigned short int);
+
+  /**
+   * @desc    LCD send 8bits instruction in 8 bit mode
+   *
+   * @param   unsigned short int
+   * @return  void
+   */
+  void HD44780_Send8bitsIn8bitMode (unsigned short int);
 
   /**
    * @desc    LCD send upper nibble
@@ -152,15 +205,15 @@
    * @param   unsigned short int
    * @return  void
    */
-  void HD44780_SendUppNibble(unsigned short int);
+  void HD44780_SetUppNibble (unsigned short int);
 
   /**
-   * @desc    LCD send
+   * @desc    LCD send lower nibble
    *
    * @param   unsigned short int
    * @return  void
    */
-  void HD44780_SendLowNibble(unsigned short int);
+  void HD44780_SetLowNibble (unsigned short int);
 
   /**
    * @desc    LCD pulse E
@@ -168,6 +221,6 @@
    * @param   void
    * @return  void
    */
-  void HD44780_PulseE(void);
+  void HD44780_PulseE (void);
 
 #endif
