fd5804b08b4104aa07f5ed95e20a66c2b7ffdde0
Marián <Matiasus@users.noreply.github.com>
2019-11-25 13:03:35 +0100

    Update hd44780.c

--- lib/hd44780.c
+++ lib/hd44780.c
@@ -8,8 +8,11 @@
  * @datum       05.11.2019
  * @file        hd44780.c
  * @tested      AVR Atmega16 / AVR Atmega8
- * @inspiration
- * ---------------------------------------------------
+ * @descript    Library designed for LCD with HD44780 driver
+ * @usage       default set 16x2 LCD / but different possibilities 1 row, 80 characters etc
+ *              4-bit or 8bit mode (4-bit mode tested under atmega16) with 3 control wires (RW, RS, E)
+ *              8-bit or 8bit mode (8-bit mode tested under atmega16) with 3 control wires (RW, RS, E)
+ * 
  */
 
 // include libraries
